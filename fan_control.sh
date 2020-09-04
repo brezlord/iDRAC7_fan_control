e8bb9d229de003863c10dc2a5160f9bd7bf77310
brezlord <32946493+brezlord@users.noreply.github.com>
2020-09-04 13:59:12 +0800

    Update fan_control.sh

--- fan_control.sh
+++ fan_control.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
+# https://github.com/brezlord/iDRAC7_fan_control
 # A simple script to control fan speeds on Dell generation 12 PowerEdge servers. 
 # If the inlet temperature is above 35deg C enable iDRAC dynamic control and exit program.
 # If inlet temp is below 35deg C set fan control to manual and set fan speed to predetermined value.
